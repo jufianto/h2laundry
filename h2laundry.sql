8c1030f9c1b8d6bbf19a625ba57f9c8a7dcb33e6
jufianto <jufiantohendri@gmail.com>
2016-12-25 15:43:10 +0700

    new database

--- h2laundry.sql
+++ h2laundry.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 28, 2016 at 08:40 AM
+-- Generation Time: Dec 25, 2016 at 09:42 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 7.0.9
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `h2laundry`
 --
+CREATE DATABASE IF NOT EXISTS `h2laundry` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `h2laundry`;
 
 -- --------------------------------------------------------
 
@@ -38,7 +40,70 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id_admin`, `nama_admin`, `no_hp_admin`, `password`) VALUES
-(1, 'admin', '081234345656', '21232f297a57a5a743894a0e4a801fc3');
+(1, 'admin', '65676767', '21232f297a57a5a743894a0e4a801fc3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_barang` int(11) NOT NULL,
+  `nama_barang` varchar(30) NOT NULL,
+  `merk_barang` varchar(30) NOT NULL,
+  `stok` int(10) NOT NULL,
+  `total_harga` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id_barang`, `nama_barang`, `merk_barang`, `stok`, `total_harga`) VALUES
+(1, 'soffel', 'Autan', 10, 30000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paket`
+--
+
+CREATE TABLE `paket` (
+  `id_paket` int(11) NOT NULL,
+  `paket` varchar(30) NOT NULL,
+  `harga` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `paket`
+--
+
+INSERT INTO `paket` (`id_paket`, `paket`, `harga`) VALUES
+(1, 'express', 7000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `id_pelgn` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `pass_pelgn` varchar(50) NOT NULL,
+  `nama_pelgn` varchar(50) NOT NULL,
+  `no_hp_pelgn` varchar(12) NOT NULL,
+  `almt_pelgn` varchar(50) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`id_pelgn`, `username`, `pass_pelgn`, `nama_pelgn`, `no_hp_pelgn`, `almt_pelgn`, `status`) VALUES
+(1, 'ayu', '29c65f781a1068a41f735e1b092546de', 'ayu', '085365861261', 'jalan tanjung batu no 130', 0);
 
 --
 -- Indexes for dumped tables
@@ -52,6 +117,24 @@ ALTER TABLE `admin`
   ADD UNIQUE KEY `nama_admin` (`nama_admin`);
 
 --
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `paket`
+--
+ALTER TABLE `paket`
+  ADD PRIMARY KEY (`id_paket`);
+
+--
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id_pelgn`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -60,6 +143,21 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `admin`
   MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `paket`
+--
+ALTER TABLE `paket`
+  MODIFY `id_paket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  MODIFY `id_pelgn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
