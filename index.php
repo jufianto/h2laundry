bdd13f8e66ca335e524f69b5b6e58d5ef9c5a0ca
jufianto <jufiantohendri@gmail.com>
2016-12-25 15:39:41 +0700

    add new index

---
+++ index.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>H2 Laundry</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="css/magnific-popup.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/creative.min.css" rel="stylesheet">
+
+    <!-- Squad theme CSS -->
+    <link href="css/footer.css" rel="stylesheet">
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">H2 Laundry</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+
+                    <li>
+                        <a class="page-scroll" href="#contact">About</a>
+                    </li>
+					<li>
+                        <a class="page-scroll" href="Register.php">Register</a>
+                    </li>
+					<li>
+						<a class="page-scroll" href="#Login">Login</a>
+					</li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <h1 id="homeHeading">Laundry Made Easy</h1>
+                <hr>
+                <p>Jasa H2 laundry sudah melayani ratusan pelanggan dari seluruh Pekanbaru. Bergabunglah dan nikmatilah pengalaman eksklusif bersama H2 Laundry.</p>
+                <a href="#about" class="btn btn-primary btn-xl page-scroll">Order Now</a>
+            </div>
+        </div>
+    </header>
+
+    <section class="bg-primary" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Order Dry Cleaning and </h2>
+					<h2 class="section-heading">Laundry Online</h2>
+                    <hr class="light">
+                    <p class="text-faded">Free Collection and Delivery <img class="img-circle" width="25" height="25"   src="img/cek.jpg"> 7 Days a Week <img class="img-circle" width="25" height="25"   src="img/cek.jpg"></p>
+					 <p class="text-faded"> Prices from just 5K/Kg <img class="img-circle" width="25" height="25"   src="img/cek.jpg"> Quality Guaranteed <img class="img-circle" width="25" height="25"   src="img/cek.jpg"></p>
+                    <a href="#services" class="page-scroll btn btn-default btn-xl sr-button">Get Started	!</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">How H2 Works</h2>
+                    <hr class="primary">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <img src="img/Order.jpg">
+                        <h3>You Order</h3>
+                        <p class="text-muted">Book a collection time online or with the app</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <img src="img/Collect.jpg">
+                        <h3>We Collect</h3>
+                        <p class="text-muted">We'll bring a bag - you don't need to do anything</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <img src="img/Clean.jpg">
+                        <h3>We Clean</h3>
+                        <p class="text-muted">Our facilities are so good we guarantee you'll be satisifed</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <img src="img/Deliver.jpg">
+                        <h3>We Deliver</h3>
+                        <p class="text-muted">We'll deliver back to you, anytime and anywhere</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="no-padding" id="portfolio">
+        <div class="container-fluid">
+            <div class="row no-gutter popup-gallery">
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/1.jpg" class="portfolio-box">
+                        <img src="img/1.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="fa fa-search fa-3x sr-contact"></div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/2.jpg" class="portfolio-box">
+                        <img src="img/2.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="fa fa-search fa-3x sr-contact"></div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/3.jpg" class="portfolio-box">
+                        <img src="img/3.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="fa fa-search fa-3x sr-contact"></div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/4.jpg" class="portfolio-box">
+                        <img src="img/4.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="fa fa-search fa-3x sr-contact"></div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/5.jpg" class="portfolio-box">
+                        <img src="img/5.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="fa fa-search fa-3x sr-contact"></div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/6.jpg" class="portfolio-box">
+                        <img src="img/6.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="fa fa-search fa-3x sr-contact"></div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <aside class="bg-primary">
+        <div class="container text-center">
+            <div class="call-to-action">
+                <h2>Harga Laundry</h2>
+				<hr class="light">
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                    </div>
+                </div>
+				<div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <img src="img/cleanprice.jpg">
+                        <h2>2K per Baju</h2>
+                        <p>Shirt Service</p>
+                    </div>
+                </div>
+				<div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <img src="img/kg.jpg">
+                        <h2>5K per Kilogram</h2>
+                        <p>Laundry Service</p>
+                    </div>
+                </div>
+			</div>
+        </div>
+    </aside>
+
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Gratis Antar Jemput</h2>
+                    <hr class="primary">
+                    <p>H2 Laundry melayani GRATIS antar jemput! Sekarang semuanya menjadi serba praktis. Anda tidak perlu repot-repot mencuci, mengeringkan, dan menyetrika pakaian Anda sendiri. Anda juga tidak perlu keluar rumah untuk menggotong-gotong pakaian Anda. Anda cukup menghubungi kami, maka kami langsung datang ke rumah Anda untuk menjemput pakaian Anda.</p>
+					<a href="#about" class="btn btn-primary btn-xl page-scroll">Daftar Sekarang!</a>
+				</div>
+
+                <div class="col-lg-4 col-lg-offset-2 text-center">
+                    <i class="fa fa-phone fa-3x sr-contact"></i>
+                    <p>123-456-6789</p>
+                </div>
+                <div class="col-lg-4 text-center">
+                    <i class="fa fa-envelope-o fa-3x sr-contact"></i>
+                    <p><a href="mailto:your-email@your-domain.com">h2laundry@gmail.com</a></p>
+                </div>
+            </div>
+        </div>
+   </section>
+	 <section class="bg-primary">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-sm-6 text-center">
+                    <img src="img/ft-logo.jpg" height="150" width="400">
+					<p>H2_laundry © 2016</p>
+                </div>
+				<div class="col-lg-4 col-sm-6 ">
+					<p><b>LOCATION</b></p>
+					<b>Jl.Delima No.123</b>
+					<b>Pekanbaru,RIAU</b>
+					<p>+123 456</p>
+					<p><b>EMAIL</b></p>
+					<p>H2_laundry@gmail.com</p>
+					<p><b>We're on social networks</b></p>
+					<ul class="company-social">
+                            <li class="social-facebook"><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                            <li class="social-twitter"><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                            <li class="social-dribble"><a href="#" target="_blank"><i class="fa fa-dribbble"></i></a></li>
+                            <li class="social-google"><a href="#" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+                        </ul>
+                </div>
+				<div class="col-lg-3 col-sm-6 ">
+                    <h2 class="section-heading"> </h2>
+					<p><b>Di Rancang Oleh:</b></p>
+					<p>BAYU EKA PAMBUDI</p>
+					<p>JUFIANTO HENRI</p>
+					<p>M ROFI SAPUTRA</p>
+					<p>RISKA ARNENI</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/scrollreveal.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/creative.min.js"></script>
+
+</body>
+
+</html>
